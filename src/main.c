62ac99d94d3806ec9ddb9e034f191227c34dc85a
gougonst <gougonst@gmail.com>
2025-07-03 00:14:17 +0800

    [NEW] Integrate display component
    - ILI9341 driver
    - GFX graphic library
    - Simple display test

--- src/main.c
+++ src/main.c
@@ -1,9 +1,13 @@
 #include "pico/stdlib.h"
 #include "hardware/uart.h"
+
+#include "ili9341.h"
+#include "gfx.h"
+
 #include "controller.h"
 #include "uart.h"
 
-#define INPUT_DELAY 10
+#define INPUT_DELAY 1000
 
 void pico_led_init() {
     gpio_init(PICO_DEFAULT_LED_PIN);
@@ -23,11 +27,22 @@ int main() {
     pico_set_led(true);
     uart_puts(UART_ID, "Pico led init finished.\r\n");
 
+    LCD_setPins(11, 13, 10, 6, 7);
+    LCD_initDisplay();
+    LCD_setRotation(0);
+    GFX_createFramebuf();
+    uart_puts(UART_ID, "LCD init finished.\r\n");
+
     controller_init();
     uart_puts(UART_ID, "Controller init finished.\r\n");
 
+    int counter = 0;
     while (true) {
         controller_poll();
+        GFX_clearScreen();
+        GFX_setCursor(100, 200);
+        GFX_printf("Hello GFX! %d\n", counter++);
+        GFX_flush();
         sleep_ms(INPUT_DELAY);
     }
 
